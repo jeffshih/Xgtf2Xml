51a753e93171e8cfc835ab89683cc71d2dc638fe
jeffshih <jeffhfs1224@gmail.com>
2017-02-24 17:08:22 +0900

    Add dataset factory for UCF101 training

---
+++ lib/datasets/factory.py
@@ -0,0 +1,55 @@
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+"""Factory method for easily getting imdbs by name."""
+
+__sets = {}
+
+from datasets.pascal_voc import pascal_voc
+from datasets.coco import coco
+""" Add UCF101, need to set the py file path"""
+from UCF101 import UCF101
+
+import numpy as np
+
+# Set up voc_<year>_<split> using selective search "fast" mode
+for year in ['2007', '2012']:
+    for split in ['train', 'val', 'trainval', 'test']:
+        name = 'voc_{}_{}'.format(year, split)
+        __sets[name] = (lambda split=split, year=year: pascal_voc(split, year))
+
+# Set up coco_2014_<split>
+for year in ['2014']:
+    for split in ['train', 'val', 'minival', 'valminusminival']:
+        name = 'coco_{}_{}'.format(year, split)
+        __sets[name] = (lambda split=split, year=year: coco(split, year))
+
+# Set up coco_2015_<split>
+for year in ['2015']:
+    for split in ['test', 'test-dev']:
+        name = 'coco_{}_{}'.format(year, split)
+        __sets[name] = (lambda split=split, year=year: coco(split, year))
+
+# Set up UCF
+devkit_path = '/home/jeff/py-faster-rcnn/data/UCF101devkit'
+
+for split in ['trainlist01','testlist01']:
+    name = 'UCF101{}'.format(split)
+    print name
+    __sets[name] = (lambda split=split: UCF101(split,devkit_path))
+
+print "run till here"
+
+def get_imdb(name):
+    """Get an imdb (image database) by name."""
+    if not __sets.has_key(name):
+        raise KeyError('Unknown dataset: {}'.format(name))
+    return __sets[name]()
+
+def list_imdbs():
+    """List all registered imdbs."""
+    return __sets.keys()
